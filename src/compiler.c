6556a02ccb4d0f0200c62aea04c716316dde618d
kamakuni <superradio86@gmail.com>
2019-05-08 07:54:31 +0900

    add != operator

--- src/compiler.c
+++ src/compiler.c
@@ -199,6 +199,11 @@ void gen(Node *node) {
         printf("  cmp rax, rdi\n");
         printf("  sete al\n");
         printf("  movzb rax, al\n");
+    case ND_NE:
+        printf("  cmp rax, rdi\n");
+        printf("  setne al\n");
+        printf("  movzb rax, al\n");
+        break;
     }
 
     printf("  push rax\n");
