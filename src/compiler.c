bb081a445ba3839e473484a05b294331e7b1ab81
kamakuni <superradio86@gmail.com>
2019-05-15 23:30:00 +0900

    add more test cases

--- src/compiler.c
+++ src/compiler.c
@@ -311,6 +311,14 @@ void runtest(){
     Vector *vec = new_vector();
     expect(__LINE__, 0, vec->len);
 
+    for (int i = 0; i < 100; i++)
+        vec_push(vec,(void *)i);
+
+    expect(__LINE__, 100, vec->len);
+    expect(__LINE__, 0, (long)vec->data[0]);
+    expect(__LINE__, 50, (long)vec->data[50]);
+    expect(__LINE__, 99, (long)vec->data[99]);
+
     printf("OK\n");
 }
 
