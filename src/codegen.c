87edcc48154451ae365d2e8006f10110409424e4
kamakuni <superradio86@gmail.com>
2019-06-05 23:48:17 +0900

    update gen function

--- src/codegen.c
+++ src/codegen.c
@@ -24,13 +24,13 @@ void gen(Node *node) {
         return;
     }
 
-    /*if (node->ty == ND_RETURN) {
+    if (node->ty == ND_RETURN) {
         gen(node);
         printf("  pop rax\n");
         printf("  mov rsp, rbp\n");
-
+        printf("  pop rbp\n");
         printf("  ret\n");
-    }*/
+    }
 
     if (node->ty == '=') {
         gen_lval(node->lhs);
