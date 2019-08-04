6d37f8a21bf49cfc1da1a047f2c9a35de2317c45
kamakuni <superradio86@gmail.com>
2019-08-04 22:03:25 +0900

    update testing codes

--- src/test.sh
+++ src/test.sh
@@ -49,31 +49,31 @@ try 3 "int main(){int a;int b;a=1;b=2;a+b;}"
 try 4 "int main(){int a;int b;int c;a=b=2;c=a+b;c;}"
 
 try 5 "int main(){return 5;}"
-try 7 "int main(){a=4+3; return a ;}"
+try 7 "int main(){int a; a=4+3; return a ;}"
 
-try 6 "int main(){foo=1;bar=2+3;return foo + bar;}"
+try 6 "int main(){int foo; int bar;foo=1;bar=2+3;return foo + bar;}"
 
 try 2 "int main(){if(1<10)2;}"
 try 2 "int main(){if(1<10){return 2;}}"
-try 2 "int main(){if(1<10){a=2;return a;}}"
-try 10 "int main(){var=1;if(1<10)var=10;var;}"
-try 1 "int main(){var=1;if(1>10)var=10;var;}"
+try 2 "int main(){if(1<10){int a;a=2;return a;}}"
+try 10 "int main(){int var; var=1;if(1<10)var=10;var;}"
+try 1 "int main(){int var; var=1;if(1>10)var=10;var;}"
 
 try 2 "int main(){if(1<10) 2; else 3;}"
 try 3 "int main(){if(11<10) 2; else 3;}"
 try 2 "int main(){if(1<10) {return 2;} else {return 3;}}"
 try 3 "int main(){if(11<10) {return 2;} else {return 3;}}"
 
-try 10 "int main(){i=0;while(i<10)i=i+1; return i;}"
-try 0 "int main(){i=0;while(i<10)if(i==0) return i; return 11;}"
-try 5 "int main(){i=0;while(i<10)if(i<5) i=i+1; else return i; return 11;}"
-try 10 "int main(){i=0;while(i<10){i=i+1;} return i;}"
+try 10 "int main(){int i; i=0;while(i<10)i=i+1; return i;}"
+try 0 "int main(){int i; i=0;while(i<10)if(i==0) return i; return 11;}"
+try 5 "int main(){int i; i=0;while(i<10)if(i<5) i=i+1; else return i; return 11;}"
+try 10 "int main(){int i; i=0;while(i<10){i=i+1;} return i;}"
 
-try 20 "int main(){a=10;for(i=0;i<10;i=i+1)a=a+1; return a;}"
-try 11 "int main(){i=0;for(;i<=10;)i=i+1;return i;}"
-try 0 "int main(){i=0;for(;i<=10;)if(i==0) return i; return 11;}"
-try 5 "int main(){i=0;for(;i<=10;)if(i<5) i=i+1; else return i; return 11;}"
-try 5 "int main(){i=0;for(;i<=10;)if(i<5) i=i+1; else return i; ret=11;}"
+try 20 "int main(){int i; a=10;for(i=0;i<10;i=i+1)a=a+1; return a;}"
+try 11 "int main(){int i; i=0;for(;i<=10;)i=i+1;return i;}"
+try 0 "int main(){int i; i=0;for(;i<=10;)if(i==0) return i; return 11;}"
+try 5 "int main(){int i; i=0;for(;i<=10;)if(i<5) i=i+1; else return i; return 11;}"
+try 5 "int main(){int i; i=0;for(;i<=10;)if(i<5) i=i+1; else return i; ret=11;}"
 
 try 10 "int func(){10;}int main(){func();}"
 try 10 "int func(){return 10;}int main(){func();}"
@@ -95,6 +95,6 @@ try 1  "int func(int a){return a = a + 1;}int main(){return func(0);}"
 
 try 55 "int fib(int n){if(n==0){return 0;} if(n==1){return 1;} return fib(n-2) + fib(n-1);}int main(){return fib(10);}"
 
-try 10 "int main(){a = 10; b = &a; return *b;}"
+try 10 "int main(){int 1; int b; a = 10; b = &a; return *b;}"
 
 echo OK
