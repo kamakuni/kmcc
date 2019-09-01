8e8f95831b394dcc6717c0805b5bfff4250c62bd
kamakuni <superradio86@gmail.com>
2019-09-02 08:18:18 +0900

    add expect_ident function

--- src/parse.c
+++ src/parse.c
@@ -446,6 +446,14 @@ int expect_number() {
     return val;
 }
 
+char *expect_ident() {
+    if (token->kind != TK_IDENT)
+        error("識別子ではありません");
+    char *s = strndup(token->str, token->len);
+    token = token->next;
+    return s;
+}
+
 bool at_eof() {
     return token->kind == TK_EOF;
 }
