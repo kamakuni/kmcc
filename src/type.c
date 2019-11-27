00d380bebbe2d5159177fb76c689275a64e113bc
kamakuni <superradio86@gmail.com>
2019-11-28 00:32:36 +0900

    add pointer_to function

--- src/type.c
+++ src/type.c
@@ -5,3 +5,10 @@ Type *int_type = &(Type){ TY_INT };
 bool is_integer(Type *ty){
   return ty->kind == TY_INT;
 }
+
+Type *pointer_to(Type *base) {
+  Type *ty = calloc(1, sizeof(Type));
+  ty->kind = TY_PTR;
+  ty->base = base;
+  return ty;
+}
