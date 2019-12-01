1f435f327efaee731fa2b00c1dbf03f9d2c7f832
kamakuni <superradio86@gmail.com>
2019-12-02 06:16:30 +1300

    add case for ND_ADD

--- src/type.c
+++ src/type.c
@@ -27,4 +27,12 @@ void add_type(Node *node) {
 
   for (Node *n = node->body; n;n = n->next)
     add_type(n);
+  for (Node *n = node->args; n; n = n->next)
+    add_type(n);
+
+  switch(node->kind) {
+  case ND_ADD:
+    node->ty = int_type;
+    return;
+  }
 }
