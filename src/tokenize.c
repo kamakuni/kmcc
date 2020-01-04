4fbc6ea3b9fe9a6907278e5943ade475fb81c245
kamakuni <superradio86@gmail.com>
2020-01-04 17:20:13 +0900

    add starts_with_reserved function

--- src/tokenize.c
+++ src/tokenize.c
@@ -81,6 +81,26 @@ bool startswith(char *p, char * q) {
   return strncmp(p, q, strlen(q)) == 0;
 }
 
+static char *starts_with_reserved(char *p) {
+  // Keyword
+  static char *kw[] = {"return", "if", "else", "while", "for"};
+
+  for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {
+    int len = strlen(kw[i]);
+    if (startswith(p, kw[i]) && !is_alnum(p[len]))
+      return kw[i];
+  }
+
+  // Multi-letter punctuator
+  static char *ops[] = {"==", "!=", "<=", ">="};
+
+  for (int i = 0; i < sizeof(ops) / sizeof(*ops); i++)
+    if (startswith(p, ops[i]))
+      return ops[i];
+
+  return NULL;
+}
+
 Token *peek(char *s){
     if (token->kind != TK_RESERVED || strlen(s) != token->len ||
         strncmp(token->str,s,token->len))
