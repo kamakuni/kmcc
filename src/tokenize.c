635ca6a29804534ea395fec30fd34aad3b9ab0f2
kamakuni <superradio86@gmail.com>
2020-01-04 17:50:18 +0900

    update tokenize function

--- src/tokenize.c
+++ src/tokenize.c
@@ -190,84 +190,48 @@ Token *tokenize(char *p) {
     Token *cur = &head;
     
     while (*p) {
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
+      // Skip whitespace characters
+      if (isspace(*p)) {
+        p++;
+        continue;
+      }
         
-        // Multi-letter punctuator
-        if (startswith(p, "==") || startswith(p, "!=") ||
-            startswith(p, "<=") || startswith(p, ">=")) {
-            cur = new_token(TK_RESERVED, cur, p, 2);
-            p = p + 2;
-            continue;
-        }
+      // Keywords or Multi-letter punctuators
+      char *kw = starts_with_reserved(p);
+      if (kw) {
+	int len = strlen(kw);
+	cur = new_token(TK_RESERVED, cur, p, len);
+	p += len;
+	continue;
+      }
+
+      // Identifier
+      if (is_alpha(*p)) {
+        char *q = p++;
+        while(is_alnum(*p))
+          p++;
+        cur = new_token(TK_IDENT, cur, q, p - q);
+        continue;
+      }
+
+      // Single-letter punctuators
+      if (ispunct(*p)) {
+        cur = new_token(TK_RESERVED, cur, p++, 1);
+        continue;
+      }
         
-        // single-letter punctuator
-        if (ispunct(*p)) {
-            cur = new_token(TK_RESERVED, cur, p, 1);
-            p++;
-            continue;
-        }
-
-        if (strncmp(p, "int", 3) == 0 && !is_alnum(p[3])){
-            cur = new_token(TK_RESERVED,cur,p,3);
-            p += 3;
-            continue;
-        }
-
-        if (strncmp(p, "if", 2) == 0 && !is_alnum(p[2])){
-            cur = new_token(TK_RESERVED,cur,p,2);
-            p += 2;
-            continue;
-        }
-
-        if (strncmp(p, "for", 3) == 0 && !is_alnum(p[3])){
-            cur = new_token(TK_RESERVED,cur,p,3);
-            p += 3;
-            continue;
-        }
-
-        if (strncmp(p, "else", 4) == 0 && !is_alnum(p[4])){
-            cur = new_token(TK_RESERVED,cur,p,4);
-            p += 4;
-            continue;
-        }
-
-        if (strncmp(p, "return", 6) == 0 && !is_alnum(p[6])){
-            cur = new_token(TK_RESERVED,cur,p,6);
-            p += 6;
-            continue;
-        }
-
-        if (strncmp(p, "while", 5) == 0 && !is_alnum(p[5])){
-            cur = new_token(TK_RESERVED,cur,p,5);
-            p += 5;
-            continue;
-        }
-
-        if (is_alpha(*p)) {
-            int i = 0;
-            while(is_alnum(p[i]))
-                i++;
-            cur = new_token(TK_IDENT,cur,p,i);
-            p += i;
-            continue;
-        }
+      // Integer literal
+      if (isdigit(*p)) {
+	cur = new_token(TK_NUM, cur, p ,0);
+	char *q = p;
+	cur->val = strtol(p, &p, 10);
+	cur->len = p - q;
+	continue;
+      }
         
-        // Integer literal
-        if (isdigit(*p)) {
-            char *start = p;
-            long val = strtol(p, &p, 10);
-            cur = new_token(TK_NUM,cur,start,p - start);
-            cur->val = val;
-            continue;
-        }
-        
-        error("トークナイズできません: %s", p);
-        exit(1);
+      error_at(p, "invalid token");
     }
     
-    new_token(TK_EOF,cur,p,0);
+    new_token(TK_EOF ,cur ,p ,0 );
     return head.next;
 }
