90f31561cdb9ce19f635bf3c38281f87798b2835
kamakuni <superradio86@gmail.com>
2020-01-07 23:10:08 +0900

    update main function

--- src/main.c
+++ src/main.c
@@ -10,28 +10,29 @@ int main(int argc, char **argv) {
   if (argc != 2)
     error("%s: invalid number of arguments", argv[0]);
     
-  // run testing codes
+  // Run testing codes.
   if (strcmp(argv[1],"-test") == 0){
     //runtest();
     return 0;
   }
 
-  // Tokenize and parse
-    user_input = argv[1];
-    token = tokenize(user_input);
-    
-    // tokens to syntax tree
-    Function *prog = program();
+  // Tokenize and parse.
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Function *prog = program();
 
-    for (Function *fn = prog; fn; fn = fn->next) {
-      int offset = 0;
-      for (Var *var = prog->locals; var; var = var->next) {
+  // Assign offsets to local variables.
+  for (Function *fn = prog; fn; fn = fn->next) {
+    int offset = 0;
+    for (VarList *vl = fn->locals; vl; vl = vl->next) {
 	offset += 8;
-	var->offset = offset;
-      }
-      fn->stack_size = offset;
+	vl->var->offset = offset;
     }
+    fn->stack_size = offset;
+  }
 
-    codegen(prog);
-    return 0;
+  // Traverse the AST to emit assembly.
+  codegen(prog);
+  
+  return 0;
 }
