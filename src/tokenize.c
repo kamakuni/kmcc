241d5bb9ead706a137590a2fd8d955ce7903e740
kamakuni <superradio86@gmail.com>
2020-01-08 23:38:24 +0900

    add int keywords

--- src/tokenize.c
+++ src/tokenize.c
@@ -83,7 +83,7 @@ bool startswith(char *p, char * q) {
 
 static char *starts_with_reserved(char *p) {
   // Keyword
-  static char *kw[] = {"return", "if", "else", "while", "for"};
+  static char *kw[] = {"return", "if", "else", "while", "for", "int"};
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {
     int len = strlen(kw[i]);
