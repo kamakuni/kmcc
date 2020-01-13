6a7c7e96cdaaee324a9f418aa40574f26ff323bd
kamakuni <superradio86@gmail.com>
2020-01-14 00:00:01 +0900

    fix gen

--- src/codegen.c
+++ src/codegen.c
@@ -58,7 +58,7 @@ void gen_block(Node *node){
 // Generatecode for given code
 static void gen(Node *node) {
 
-    if(node->kind == ND_CALL) {
+    if(node->kind == ND_FUNCALL) {
       int nargs = 0;
       for (Node *arg = node->args; arg; arg = arg->next) {
 	gen(arg);
