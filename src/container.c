86ed3cb3c79cd05ed4448f48a24208e91810a64a
kamakuni <superradio86@gmail.com>
2020-01-26 22:59:37 +0900

    delete unnecessary codes from contaner.c

--- src/container.c
+++ src/container.c
@@ -43,7 +43,6 @@ Var *new_var(){
     Var *var = malloc(sizeof(Var));
     var->next = NULL;
     var->ty = NULL;
-    var->name = "";
     var->offset = 0;
     return var;
 }
@@ -52,43 +51,8 @@ void var_insert_first(Var **var, Type *ty, char *name, int offset){
     Var *new = malloc(sizeof(Var));
     new->next = *var;
     new->ty = ty;
-    new->name = name;
     new->offset = offset;
     *var = new;
-}
-
-int var_exist(Var *var, char *name){
-    while(var->next != NULL){
-        if(strcmp(var->name, name) == 0){
-            return 1;
-        }
-        var = var->next;
-    };
-    return 0;
-}
-
-Var *var_get(Var *var,char *name) {
-    while(var->next != NULL){
-        if(strcmp(var->name, name) == 0){
-            Var *res = new_var();
-            res->ty = var->ty;
-            res->name = var->name;
-            res->offset = var->offset;
-            return res;
-        }
-        var = var->next;
-    };
-    return NULL;
-}
-
-int var_get_offset(Var *var, char *name){
-    while(var->next != NULL){
-        if(strcmp(var->name, name) == 0){
-            return var->offset;
-        }
-        var = var->next;
-    };
-    return 0;
 }
 
 int var_len(Var *var){
@@ -99,14 +63,6 @@ int var_len(Var *var){
     };
     return i;
 }
-
-/*void var_append(Var *var, Type *ty, char *name){
-    int offset = (var_len(variables) + 1) * 8;
-    int current = var_get_offset(variables, name);
-    if (current != 0)
-        offset = current;
-    var_insert_first(&variables, ty, name, offset);
-    }*/
 
 Tokens *new_tokens(){
     Tokens *t = malloc(sizeof(Tokens));
@@ -122,86 +78,3 @@ Token *get(Tokens *t, int i) {
     return (Token *) t->vec->data[i];
 }
 
-/*
-int expect(int line, int expected, int actual) {
-    if (expected == actual)
-        return 0;
-    fprintf(stderr,"%d: %d expected, but got %d\n",line, expected, actual);
-    exit(1);
-}
-*/
-/*
-void test_map() {
-    Map *map = new_map();
-    expect(__LINE__, 0,(long)map_get(map,"foo"));
-    
-    map_put(map, "foo", (void *)2);
-    expect(__LINE__, 2,(long)map_get(map,"foo"));
-    
-    map_put(map, "bar", (void *)3);
-    expect(__LINE__, 3,(long)map_get(map,"bar"));
-    
-    map_put(map, "foo", (void *)4);
-    expect(__LINE__, 4,(long)map_get(map,"foo"));
-}
-*/
-/*
-void test_vector(){
-    Vector *vec = new_vector();
-    expect(__LINE__, 0, vec->len);
-    
-    for (int i = 0; i < 100; i++)
-        vec_push(vec,(void *)i);
-    
-    expect(__LINE__, 100, vec->len);
-    expect(__LINE__, 0, (long)vec->data[0]);
-    expect(__LINE__, 50, (long)vec->data[50]);
-    expect(__LINE__, 99, (long)vec->data[99]);
-    
-    Tokens *tokens = new_tokens();
-    append(tokens, new_token_num(2,"3"));
-    Token *token = get(tokens,0);
-    
-    expect(__LINE__, TK_NUM, token->kind);
-    expect(__LINE__, 2, token->val);
-    expect(__LINE__, 0, strcmp("3", token->input));
-    
-    append(tokens, new_token_num(3,"4"));
-    
-    token = get(tokens,1);
-    
-    expect(__LINE__, TK_NUM, token->kind);
-    expect(__LINE__, 3, token->val);
-    expect(__LINE__, 0, strcmp("4", token->input));*
-
-}
-*/
-/*
-void test_linked_list(){
-    Var *var = new_var();
-    expect(__LINE__, 0, var->offset);
-    expect(__LINE__, 0, strcmp("", var->name));
-    Type *ty = malloc(sizeof(Type));
-    ty->ty = INT;
-    var_insert_first(&var, ty, "name1", (var_len(var)+1)*8);
-    expect(__LINE__, 8, var->offset);
-    expect(__LINE__, INT, var->ty->ty);
-    expect(__LINE__, 0, strcmp("name1", var->name));
-    ty = malloc(sizeof(Type));
-    ty->ty = PTR;
-    var_insert_first(&var, ty, "name2", (var_len(var)+1)*8);
-    expect(__LINE__, 16, var->offset);
-    expect(__LINE__, PTR, var->ty->ty);
-    expect(__LINE__, 0, strcmp("name2", var->name));
-    int offset = var_get_offset(var, "name1");
-    expect(__LINE__, 8, offset);
-}
-*/
-/*
-void runtest(){
-    test_vector();
-    test_map();
-    test_linked_list();
-    printf("OK\n");
-}
-*/
