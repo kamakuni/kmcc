5032885446dd8cbb921978463f1b26cb4db8f36b
kamakuni <superradio86@gmail.com>
2020-02-02 16:12:38 +0900

    update pointer_to function

--- src/type.c
+++ src/type.c
@@ -1,6 +1,6 @@
 #include "kmcc.h"
 
-Type *int_type = &(Type){ TY_INT };
+Type *int_type = &(Type){ TY_INT, 4 };
 
 bool is_integer(Type *ty){
   return ty->kind == TY_INT;
@@ -10,6 +10,7 @@ Type *pointer_to(Type *base) {
   Type *ty = calloc(1, sizeof(Type));
   ty->kind = TY_PTR;
   ty->base = base;
+  ty->size = 8;
   return ty;
 }
 
