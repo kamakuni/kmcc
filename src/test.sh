cdb3634c3e3a0e4822f8b8eb28408f8640a4f977
Sugawara Kensuke <superradio86@gmail.com>
2020-10-24 14:31:10 +0000

    Add more test codes for Initializing array

--- src/test.sh
+++ src/test.sh
@@ -203,4 +203,8 @@ try 0 'int main() {int x[3]={}; x[0];}'
 try 0 'int main() {int x[3]={}; x[1];}'
 try 0 'int main() {int x[3]={}; x[2];}'
 
+try 2 "int main() {int x[2][3]={{1,2}}; x[0][1]; }"
+try 0 "int main() {int x[2][3]={{1,2}}; x[1][0]; }"
+try 0 "int main() {int x[2][3]={{1,2}}; x[1][2]; }"
+
 echo OK
