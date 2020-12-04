d29025a65316a233c65cd0d5eec0b8280890914e
Sugawara Kensuke <superradio86@gmail.com>
2020-12-04 14:56:24 +0000

    Add Typefor short and long

--- src/kmcc.h
+++ src/kmcc.h
@@ -242,7 +242,9 @@ struct Member {
 
 extern Type *void_type;
 extern Type *char_type;
+extern Type *short_type;
 extern Type *int_type;
+extern Type *long_type;
 
 bool is_integer(Type *ty);
 int align_to(int n, int align);
