1ec3975cf56dc90743d36ab6e8d5b7ec76ea87d8
Sugawara Kensuke <superradio86@gmail.com>
2021-03-30 14:38:35 +0000

    Add _Alignof keyword

--- src/tokenize.c
+++ src/tokenize.c
@@ -89,7 +89,7 @@ bool startswith(char *p, char * q) {
 
 static char *starts_with_reserved(char *p) {
   // Keyword
-  static char *kw[] = {"return", "if", "else", "while", "for", "short", "int", "long", "char", "sizeof", "struct", "void", "typedef", "_Bool", "enum", "static", "extern"};
+  static char *kw[] = {"return", "if", "else", "while", "for", "short", "int", "long", "char", "sizeof", "struct", "void", "typedef", "_Bool", "enum", "static", "extern", "_Alignof"};
 
   for (int i = 0; i < sizeof(kw) / sizeof(*kw); i++) {
     int len = strlen(kw[i]);
