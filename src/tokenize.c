d50ad79bd22820cdf26d4a9d1e84834c7b2c0cba
Sugawara Kensuke <superradio86@gmail.com>
2021-05-20 12:15:52 +0000

    Update read_int_literal function

--- src/tokenize.c
+++ src/tokenize.c
@@ -275,11 +275,25 @@ static Token *read_int_literal(Token *cur, char *start) {
   }
 
   long val = strtol(p, &p, base);
+  Type *ty = int_type;
+
+  // Read L or LL prefix or infer a type.
+  if (startswith(p, "LL") || startswith(p, "ll")) {
+    p += 2;
+    ty = long_type;
+  } else if (*p == 'L' || *p == 'l') {
+    p++;
+    ty = long_type;
+  } else if (val != (int)val) {
+    ty = long_type;
+  }
+
   if (is_alnum(*p))
     error_at(p, "invalid digit");
 
   Token *tok = new_token(TK_NUM, cur, start, p - start);
   tok->val = val;
+  tok->ty = ty;
   return tok;
 }
 
